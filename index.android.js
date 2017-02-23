4c5fa2ad717127b885c0c6cb6d105831b4457432
Vinh Tran <vinh.tran02@pyramid-consulting.com>
2017-02-23 16:19:43 +0700

    update example

--- index.android.js
+++ index.android.js
@@ -13,20 +13,44 @@ import {
 } from 'react-native';
 
 class Greeting extends Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {isShowText : true}
+
+      setInterval(() => {
+        this.setState({isShowText : !this.state.isShowText})
+      }, 1000);
+    }
+
+
     render() {
+        let display = this.state.isShowText ? this.props.textContent : ' ';
+
         return (
-            <Text>Hello {this.props.username}</Text>
+            <Text style={myColorStyle.red}>{display}</Text>
         );
     }
 }
 
+const myColorStyle = StyleSheet.create({
+  red: {
+    fontWeight : 'bold',
+    fontSize : 30,
+    color : '#F00'
+  }
+})
 
 export default class HelloWorldApp extends Component {
   render() {
       return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting username="Vinh 1" />
-        <Greeting username="Vinh 2" />
+      <View style={{
+          flex : 1,
+          flexDirection: 'row',
+          alignItems: 'center'}}>
+        <View style={{flex : 1, backgroundColor: '#CCC'}}><Greeting textContent="Hello world!!!" /></View>
+        <View style={{flex : 1, backgroundColor: '#0F0'}}><Greeting textContent="What the hell!!!" /></View>
       </View>
       );
     }
